8f4e8b83eead2ca4f0c70050a860da60b9261307
Krygsm <lodomirfiol@gmail.com>
2024-03-11 22:53:34 +0100

    3 buttons

--- app/src/main/java/com/example/schoolapp/MainActivity.kt
+++ app/src/main/java/com/example/schoolapp/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
         val button3: Button = findViewById(R.id.button3)
 
         button.setOnClickListener {
-            textView.text = "Kliknales"
+            textView.text = "1"
         }
         button2.setOnClickListener {
             textView.text = "2"
