1ee86b78e196c7e9da0439bc73dada42c1d0795e
Krygsm <lodomirfiol@gmail.com>
2024-03-11 22:51:46 +0100

    3 buttons

--- app/src/main/java/com/example/schoolapp/MainActivity.kt
+++ app/src/main/java/com/example/schoolapp/MainActivity.kt
@@ -4,15 +4,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.schoolapp.ui.theme.SchoolAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +12,18 @@ class MainActivity : ComponentActivity() {
 
         val textView: TextView = findViewById(R.id.textView)
 
-        val button: Button = findViewById(R.id.button)
+        val button1: Button = findViewById(R.id.button)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
 
         button.setOnClickListener {
             textView.text = "Kliknales"
+        }
+        button2.setOnClickListener {
+            textView.text = "2"
+        }
+        button3.setOnClickListener {
+            textView.text = "3"
         }
     }
 }
