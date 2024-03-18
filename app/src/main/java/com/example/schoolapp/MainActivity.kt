da814838ff890183a812ee30ef7c4b55d001ba42
Krygsm <lodomirfiol@gmail.com>
2024-03-18 20:59:32 +0100

    Calculator1

--- app/src/main/java/com/example/schoolapp/MainActivity.kt
+++ app/src/main/java/com/example/schoolapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.schoolapp
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -9,21 +10,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
+    }
 
-        val textView: TextView = findViewById(R.id.textView)
+    fun appendText(view: View) {
+        val button = view as Button
+        val buttonText = button.text.toString()
 
-        val button1: Button = findViewById(R.id.button)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
+        val textView = findViewById<TextView>(R.id.textView8)
+        var currentText = textView.text.toString()
 
-        button.setOnClickListener {
-            textView.text = "11"
-        }
-        button2.setOnClickListener {
-            textView.text = "2"
-        }
-        button3.setOnClickListener {
-            textView.text = "3"
-        }
+        // Append the button text to the current text in the TextView
+        currentText += buttonText
+        textView.text = currentText
     }
 }
