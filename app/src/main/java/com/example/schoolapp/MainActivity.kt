6a499cb9a667175358a6e74c93578dd0f7f21eec
Krygsm <lodomirfiol@gmail.com>
2024-06-03 22:56:44 +0200

    Calculator3

--- app/src/main/java/com/example/schoolapp/MainActivity.kt
+++ app/src/main/java/com/example/schoolapp/MainActivity.kt
@@ -8,6 +8,7 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : ComponentActivity() {
     private lateinit var txtScreen: TextView
+    private var appendDot: Boolean = false;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
@@ -29,7 +30,7 @@ class MainActivity : ComponentActivity() {
             findViewById<Button>(R.id.Subdivide),
             findViewById<Button>(R.id.Multiply),
             findViewById<Button>(R.id.Divide),
-            findViewById<Button>(R.id.Modulo)
+            findViewById<Button>(R.id.Modulo),
         )
 
         for (button in buttons) {
@@ -48,12 +49,35 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun appendText(text: String) {
+        if(appendDot == true && (text == "+" || text == "-" || text == "*" || text == "/" || text == "%")) { appendDot = false; }
+        if(appendDot == true) { txtScreen.append(".") }
+        val currentText = txtScreen.text.toString()
+        if (currentText.isNotEmpty()) {
+            val lastChar = currentText.last()
+
+
+            if ((lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/' || lastChar == '%') && text == "0") {
+                appendDot = true;
+            } else {
+                appendDot = false;
+            }
+        } else {
+            if(text == "0")
+            {
+                appendDot = true;
+            }
+            else if(text == ".")
+            {
+                appendText("0")
+            }
+        }
+
         txtScreen.append(text)
     }
 
     private fun calculate() {
         val expressionString = txtScreen.text.toString()
-        if (expressionString.contains("/0")) {
+        if (expressionString.contains(Regex("/0(?!\\.)"))) {
             txtScreen.text = "Undefined"
         } else {
             try {
